254ff86a6a3254ae99dadc9a6813c6e0237ced1d
VoThanhLoc <vothanhloc2201@gmail.com>
2025-03-06 15:17:34 +0700

    complete homepage demo

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const menuTrigger = document.querySelector('.menu-trigger');
-    const menu = document.querySelector('.menu');
-
-    menuTrigger.addEventListener('click', () => {
-        menu.style.opacity = menu.style.opacity === '1' ? '0' : '1';
-        menu.style.visibility = menu.style.visibility === 'visible' ? 'hidden' : 'visible';
-    });
-
     // Auto slider
-    const slides = document.querySelectorAll('.splide__slide');
+    const slides = document.querySelectorAll('.slide__slide');
     let currentSlide = 0;
 
     function showSlide(index) {
@@ -23,4 +15,25 @@ document.addEventListener('DOMContentLoaded', function () {
     }
     showSlide(currentSlide);
     setInterval(nextSlide, 9000);
+
+    
 });
+
+function embedIframe() {
+    document.getElementById('embed-area').innerHTML = `
+        <iframe class="w-100 h-100" src="https://www.youtube.com/embed/LygvirdTB3s?autoplay=1" title="Nha Trang Flycam 2021 - Mavic Air 2 4K | Nào Ta Cùng Đi" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`;
+}
+function toggleMenu() {
+    const menu = document.querySelector('.menu');
+    menu.style.opacity = menu.style.opacity === '1' ? '0' : '1';
+    menu.style.transform = menu.style.transform === 'translateX(0px)' ? 'translateX(20px)' : 'translateX(0px)';
+    menu.style.visibility = menu.style.visibility === 'visible' ? 'hidden' : 'visible';
+    // menu.style.width = menu.style.width === '400px' ? '0' : '400px';
+    if (menu.style.width != '0px' &&  menu.style.width != '')
+    {
+        setTimeout(()=>menu.style.width='0',500)
+    }
+    else {
+        menu.style.width = 'min(400px,100vw)'
+    }
+}
